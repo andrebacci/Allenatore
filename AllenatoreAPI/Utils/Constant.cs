3c199edaafe59c95338c6b5cc83d086ea6ff2652
Andrea Bacigalupo <AndreaBacigalupo@Air-di-Marta.station>
2020-04-24 19:25:16 +0200

    BE - Aggiunta classe per le costanti

---
+++ AllenatoreAPI/Utils/Constant.cs
@@ -0,0 +1,17 @@
+namespace AllenatoreAPI.Utils
+{
+    public static class Constant
+    {
+        public static readonly int FEET_RIGHT = 1;
+
+        public static readonly int FEET_LEFT = 2;
+
+        public static readonly int CARD_YELLOW = 1;
+
+        public static readonly int CARD_RED = 2;
+
+        public static readonly int TRANSFER_SESSION_SUMMER = 1;
+
+        public static readonly int TRANSFER_SESSION_WINTER = 2;
+    }
+}
