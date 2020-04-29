2eafead5f99633600d47fc8ed2c3fc4892efe99c
Andrea Bacigalupo <AndreaBacigalupo@MacBook-Air-di-Marta.local>
2020-04-29 21:32:24 +0200

    Code cleaning

--- FrontEnd/ClientApp/src/app/app.module.ts
+++ FrontEnd/ClientApp/src/app/app.module.ts
@@ -9,8 +9,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { GameComponent } from './game/game.component';
 import { GameInfoComponent } from './game-info/game-info.component';
 import { PlayerComponent } from './player/player.component';
@@ -38,8 +36,6 @@ import { UtilityService } from '../services/utility.service';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     GameComponent,
     GameInfoComponent,
     PlayerComponent,
