d356f521ef5ebe353da0ed3630466076dc8891d5
Andrea Bacigalupo <AndreaBacigalupo@MacBook-Air-di-Marta.local>
2020-05-01 17:51:58 +0200

    Game-Info - WIP recursive dropdown

--- FrontEnd/ClientApp/src/app/game-info/game-info.component.ts
+++ FrontEnd/ClientApp/src/app/game-info/game-info.component.ts
@@ -26,10 +26,11 @@ export class GameInfoComponent {
 
   idGame: number = 0;
 
-  listPlayerHome: number[] = [20];
+  numbers;
 
   constructor(private gameService: GameService, private playerService: PlayerService, private route: ActivatedRoute, private router: Router) {
-
+    //this.numbers = Array(5).fill().map((x,i)=>i);
+    this.numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
   }
 
   ngOnInit(): void {
