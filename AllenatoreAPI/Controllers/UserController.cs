a0c305a4b662e7ac60926f2ff50bf8fa7ca5cfc6
Andrea Bacigalupo <AndreaBacigalupo@MacBook-Air-di-Marta.local>
2020-05-06 21:03:29 +0200

    Aggiunto user controller

---
+++ AllenatoreAPI/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+using AllenatoreAPI.Models;
+using AllenatoreAPI.Result;
+using AllenatoreAPI.Utils;
+using BusinessLogic;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using OfficeOpenXml;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Math;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace AllenatoreAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        private readonly string _connectionString;
+
+        public UserController()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            _configuration = builder.Build();
+
+            _connectionString = _configuration.GetValue<string>("ConnectionString");
+        }        
+    }
+}
