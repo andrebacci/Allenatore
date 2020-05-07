a04216d6f0129e61045b0d4219299ab56bdc2f12
CicciTrvek83 <62645590+CicciTrvek83@users.noreply.github.com>
2020-05-07 08:56:39 +0200

    Spero di non fare casini

--- DataAccess/Models/POContext.cs
+++ DataAccess/Models/POContext.cs
@@ -31,8 +31,9 @@ namespace DataAccess.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=Allenatore;User Id=andre;Password=123Stella$;");
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS17;Database=Allenatore;User Id=sa;Password=123Stella$;");
             }
+            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
